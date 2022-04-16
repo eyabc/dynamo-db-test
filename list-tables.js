da82e492ef29e657db3dd2a5b80dd1a748b24da3
eyabc <bey4314@naver.com>
2022-04-16 19:40:07 +0900

    feat: create dynamoDB table with sdk

--- list-tables.js
+++ list-tables.js
@@ -1,19 +1,50 @@
 const AWS = require('aws-sdk');
 AWS.config.update({
-  region: '...',
-  credentials: {
-    accessKeyId: '...',
-    secretAccessKey: '...'
-  },
-
+  region: 'us-east-1',
 });
 
 const dynamodb = new AWS.DynamoDB();
 
-dynamodb.listTables((err, data) => {
+const callback = (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(JSON.stringify(data, null, 2));
   }
-});
+};
+
+dynamodb.listTables({}, callback);
+
+dynamodb.describeTable({
+  TableName: 'td_notes',
+}, callback);
+
+dynamodb.createTable({
+    TableName: 'td_notes_sdk',
+    AttributeDefinitions: [
+      {
+        AttributeName: 'user_id',
+        AttributeType: 'S',
+      },
+      {
+        AttributeName: 'timestamp',
+        AttributeType: 'N',
+      },
+    ],
+    KeySchema: [
+      {
+        AttributeName: 'user_id',
+        KeyType: 'HASH', // Partition Key
+      },
+      {
+        AttributeName: 'timestamp',
+        KeyType: 'RANGE',  // Sort Key
+      },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 1,
+      WriteCapacityUnits: 1
+    }
+  },
+  callback,
+);
